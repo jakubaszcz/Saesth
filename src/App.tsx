cc4f1eadf0f332a1befca737786bfd01df5dfe9d
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-20 12:55:36 +0200

    feat(frontend): fetch and display the song structure from the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+interface SoundData {
+  play: boolean;
+  path: string;
+}
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+type SoundMap = Record<string, SoundData>;
+
+function App() {
+  const [sounds, setSounds] = useState<SoundMap>({});
+
+  useEffect(() => {
+    async function fetchSounds() {
+      try {
+        const fetchedSounds = await invoke<SoundMap>("get_sounds");
+        setSounds(fetchedSounds);
+      } catch (error) {
+        console.error("Failed loading songs :", error);
+      }
+    }
+    fetchSounds();
+  }, []);
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
       <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>Available songs :</h3>
+        <ul style={{ textAlign: 'left' }}>
+          {Object.entries(sounds).map(([id, data]) => (
+            <li key={id}>
+              <strong>{id}</strong>: {data.path} {data.play ? " (Playing)" : " (Stop)"}
+            </li>
+          ))}
+        </ul>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
