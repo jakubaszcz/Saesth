38cdb56ce0bc8f82aea9eb9298e11a0a73f6cc72
jakub <jakub.szczucinski@epitech.eu>
2026-09-04 09:00:06 +0200

    feat: style the pack footer

--- src/containers/packs/ContainerPacks.tsx
+++ src/containers/packs/ContainerPacks.tsx
@@ -4,15 +4,21 @@ export function ContainerPacks() {
     const { packs, openPack } = usePacks();
 
     return (
-        <div>
-            <button onClick={openPack}>Open</button>
-            <div>
-                {
-                    packs.map((pack,) => (
-                        <p>{pack.name}</p>
-                    ))
-                }
+        <div className="h-full w-full flex flex-col">
+            <div className="flex-1">
+                {packs.map((pack) => (
+                    <p key={pack.name}>{pack.name}</p>
+                ))}
             </div>
+
+            <footer className="w-full p-(--padding-md) flex mt-auto">
+                <button
+                    onClick={openPack}
+                    className="ml-auto text-primary-500 bg-primary-800 p-2 w-30 rounded-xl transition duration-300 hover:text-primary-400"
+                >
+                    Open
+                </button>
+            </footer>
         </div>
-    )
+    );
 }
